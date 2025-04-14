af57e67fc7f4a5cb69915ebc853ecd9cb27167ea
abhiram1809 <140681337+hasithaa02@users.noreply.github.com>
2025-04-14 14:55:15 +0530

    Added a flask backend that uses the model
    Added a flask backend that uses the model and gives a prediction based on user inputs with the /predict endpoint
    
    Also added a requirmeents.txt file for environment replication

---
+++ app.py
@@ -0,0 +1,58 @@
+import os
+from flask import Flask, render_template, request
+import numpy as np
+import joblib
+
+# Fixed path
+fullpath = r'C:\Users\hasit\OneDrive\Desktop\Heart_Disease_prediction-WebApp\Heart-Disease-Prediction-main\hdp_model.pkl'
+
+
+app = Flask(__name__)
+model = joblib.load(fullpath)
+
+
+@app.route("/",)
+def hello():
+    return render_template("index.html")
+
+
+@app.route("/detail", methods = ["POST"])
+def submit():
+    # Html to py
+    if request.method == "POST":
+        name = request.form["Username"]
+
+    return render_template("detail.html", n = name)
+
+
+@app.route('/predict', methods = ["POST"])
+def predict():
+    if request.method == "POST":
+        
+        age = int(request.form['age'])
+        sex = int(request.form['sex'])
+        cp = int(request.form['cp'])
+        trestbps = int(request.form['trestbps'])
+        chol = int(request.form['chol'])
+        fbs = int(request.form['fbs'])
+        restecg = int(request.form['restecg'])
+        thalach = int(request.form['thalach'])
+        exang = int(request.form['exang'])
+        oldpeak = float(request.form['oldpeak'])
+        slope = int(request.form['slope'])
+        ca = int(request.form['ca'])
+        thal = int(request.form['thal'])
+
+        values = np.array([[age,sex,cp,trestbps,chol,fbs,restecg,thalach,exang,oldpeak,slope,ca,thal]])
+        prediction = model.predict(values)
+        
+
+    
+
+
+        return render_template('predict.html', prediction=prediction)
+
+
+
+if __name__=="__main__":
+    app.run(debug=True)
